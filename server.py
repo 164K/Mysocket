5591112e15d9347e34fdb673764e12f83e981d1a
164k <948165356@qq.com>
2022-10-11 05:49:38 +0000

    First commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from socket import *
+
+IP = '127.0.0.1'
+PORT = 60000
+BUFLEN = 1024
+
+listen_socket = socket(AF_INET, SOCK_STREAM)
+
+listen_socket.bind((IP, PORT))
+
+listen_socket.listen(5)
+
+print(f'Server: Service is running on port {PORT}. Waiting for client connection...')
+
+
+data_socket , ip_port = listen_socket.accept()
+print(f'Server >>> Accept a client {ip_port} connection')
+
+while True:
+    recved = data_socket.recv(BUFLEN)
+
+    if not recved:
+        break
+
+    info = recved.decode('utf-8')
+
+    print(f'Server >>> Received from client: \n {info}')
+
+    data_socket.send(f'Server >>> Server{(IP, PORT)} received the info\n{info}'.encode('utf-8'))
