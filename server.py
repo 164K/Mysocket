d5a320e74ef659ff9ddee4d159bb5dd223c39339
164k <948165356@qq.com>
2022-10-11 06:01:56 +0000

    Copy from zhihu

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ while True:
     recved = data_socket.recv(BUFLEN)
 
     if not recved:
+        print("Client close the connection.")
         break
 
     info = recved.decode('utf-8')
@@ -27,3 +28,7 @@ while True:
     print(f'Server >>> Received from client: \n {info}')
 
     data_socket.send(f'Server >>> Server{(IP, PORT)} received the info\n{info}'.encode('utf-8'))
+
+
+data_socket.close()
+listen_socket.close()
