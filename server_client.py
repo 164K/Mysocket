29a6509de9a8c0a8b2f5ce06e58d004066ded867
164K <948165356@qq.com>
2022-10-23 03:38:30 +0800

    Done_nearly

--- server_client.py
+++ server_client.py
@@ -1,4 +1,5 @@
 from socket import *
+from guietta import Gui
 import threading
 import asyncio
 import typing
@@ -9,7 +10,7 @@ LOCALHOST = '127.0.0.1'
 PORT = 60000
 BUFLEN = 1024
 
-def send(send_socket: socket, to_name: str):
+def send(gui : Gui, send_socket: socket, to_name: str):
     while(True):
         send_msg = input()
         if send_msg == 'exit':
@@ -18,7 +19,7 @@ def send(send_socket: socket, to_name: str):
         # print(f"[Send] Send \"{send_msg}\" to {to_name}.")
     send_socket.close()
 
-def recv(recv_socket: socket, from_name: str):
+def recv(gui : Gui, recv_socket: socket, from_name: str):
     while(True):
         recv_msg = recv_socket.recv(BUFLEN)
         if not recv_msg:
@@ -26,35 +27,32 @@ def recv(recv_socket: socket, from_name: str):
         print(" "*15,"<"+from_name.capitalize()+">", recv_msg.decode('utf-8'))
     recv_socket.close()
 
-def run_server(ip=LOCALHOST, port=PORT):
+def run_server(gui : Gui, ip=LOCALHOST, port=PORT):
 	listen_socket = socket(AF_INET, SOCK_STREAM)
 	listen_socket.bind((ip, port))
 	listen_socket.listen(2)
-	print(f"[Listen] {ip} is waiting for connection on PORT {port}...")
+	def func():
+		gui.status = "Listening..."
+	gui.execute_in_main_thread(func)
 	send_socket, (send_IP, send_PORT) = listen_socket.accept()
-	# print(f"Accept a client from {target_IP} on PORT {target_PORT}.")
-	# print(f"[Receive Socket] {IP} is waiting for connection on PORT {PORT}...")
 	recv_socket, (recv_IP, recv_PORT) = listen_socket.accept()
-	# print(f"Accept a client from {target_IP} on PORT {target_PORT}.")
 
-	print("-"*30,"Connected", "-"*30)
+	def func():
+		gui.status = "Connected!"
+	gui.execute_in_main_thread(func)
 	
-	thr1 = threading.Thread(target=send, args=(send_socket, 'client'))
-	thr2 = threading.Thread(target=recv, args=(recv_socket, 'client'))
+	while IS_CONNECT:
+		time.sleep(.2)
+		pass
 
-	thr1.start()
-	thr2.start()
-
-	while True:
-		if not thr1.is_alive() and not thr2.is_alive():
-			break
-
+	def func():
+		gui.status = "DisConnected"
+	gui.execute_in_main_thread(func)
 	listen_socket.close()
 	send_socket.close()
 	recv_socket.close()
-	print("-"*30, "Disconnected", "-"*30)
-
-def run_client(ip=LOCALHOST, port = PORT):
+	
+def run_client(gui, ip=LOCALHOST, port = PORT):
 	recv_socket = socket(AF_INET, SOCK_STREAM)
 	send_socket = socket(AF_INET, SOCK_STREAM)
 	time_wait = 0
