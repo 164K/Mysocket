21e57ec4ee9b31a8b450041a094eb70af864c00e
164K <948165356@qq.com>
2022-10-23 03:38:36 +0800

    Done_Nearly

---
+++ connectLinux.py
@@ -0,0 +1,2 @@
+from server_client import *
+run_server('192.168.31.225')
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
-import threading
-import typing
-import time
+import tkinter as tk 
 
-def simple_func(x: int):
-    print("Start")
-    time.sleep(1)
-    return x
+root = tk.Tk()
 
-th = threading.Thread(target=simple_func, args=(1,))
-th.start()
-time.sleep(2)
-print(th.is_alive())
+#创建主框架frame
+frame = tk.Frame(root, height=300, width=300, bg='blue', relief='raised',bd=3)
+frame.pack(padx=10, pady=10)
+ 
+#创建第二层框架frame_l和frame_r，分别左右放置在主框架frame上
+frame_t = tk.Frame(frame, height=100, width=50, bg='red')
+frame_b = tk.Frame(frame, height=100, width=50, bg='yellow')
+frame_t.pack(side='top',padx=20, pady=20)
+frame_b.pack(side='bottom',padx=20, pady=20)
+ 
+# #创建多个标签，分别放置在第二层的frame_l、frame_r上
+tk.Label(frame_t, text='左上', bg='white').pack(padx=10, pady=10)
+tk.Label(frame_t, text='左下', bg='green').pack(padx=10, pady=10)
+tk.Label(frame_b, text='右上', bg='purple').pack(padx=10, pady=10)
+tk.Label(frame_b, text='右下', bg='pink').pack(padx=10, pady=10)
+ 
+root.mainloop()
